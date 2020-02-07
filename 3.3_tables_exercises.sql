b509658fea017e93dce504f53c1e88cf06105369
Noah Melngailis <noah.melngailis@gmail.com>
2020-02-07 15:39:11 -0600

    review and changes to exercise 3.3

--- 3.3_tables_exercises.sql
+++ 3.3_tables_exercises.sql
@@ -3,6 +3,7 @@ USE employees;
 #4
 SHOW TABLES;
 #5
+DESCRIBE employees;
 # CHAR, VARCHAR, INT, DATE, ENUM
 #6
 # The tables that contain numeric type columns are: "dept_emp", "dept_manager", "employees", "employees_with_departments", "salaries", "titles"
@@ -11,8 +12,8 @@ SHOW TABLES;
 #8 
 # The tables that contain a date type are "dept_emp", "dept_manager", "employees_with_departments", "salaries", and "titles" and "employees"
 #9
-# The relationship between "employees" and "department" is the table "dept_emp" where the columns "emp_no" and "dept_no" are linked.
+# There is no direct relationship.  However, the relationship between "employees" and "department" is the table "dept_emp" where the columns "emp_no" and "dept_no" are linked.
 #10 
 SHOW CREATE TABLE dept_manager;
 dept_manager	
-CREATE TABLE `dept_manager` (↵  `emp_no` INT(11) NOT NULL,↵  `dept_no` CHAR(4) NOT NULL,↵  `from_date` DATE NOT NULL,↵  `to_date` DATE NOT NULL,↵  PRIMARY KEY (`emp_no`,`dept_no`),↵  KEY `dept_no` (`dept_no`),↵  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,↵  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE↵) ENGINE=INNODB DEFAULT CHARSET=latin1
+-- CREATE TABLE `dept_manager` (↵  `emp_no` INT(11) NOT NULL,↵  `dept_no` CHAR(4) NOT NULL,↵  `from_date` DATE NOT NULL,↵  `to_date` DATE NOT NULL,↵  PRIMARY KEY (`emp_no`,`dept_no`),↵  KEY `dept_no` (`dept_no`),↵  CONSTRAINT `dept_manager_ibfk_1` FOREIGN KEY (`emp_no`) REFERENCES `employees` (`emp_no`) ON DELETE CASCADE,↵  CONSTRAINT `dept_manager_ibfk_2` FOREIGN KEY (`dept_no`) REFERENCES `departments` (`dept_no`) ON DELETE CASCADE↵) ENGINE=INNODB DEFAULT CHARSET=latin1
