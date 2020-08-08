fc085168273e16ef3cc1138923995d964786f061
Noah Melngailis <noah.melngailis@gmail.com>
2020-08-07 22:03:00 -0500

    review folder

---
+++ sql_review/3.1_mysql_introduction.sql
@@ -0,0 +1,9 @@
+10.  Run the following queries:
+
+SELECT * FROM mysql.user;
+
+SELECT user, host FROM mysql.user
+
+SELECT * FROM mysql.help_topic
+
+SELECT help_topic_id, help_category_id, url FROM mysql.help_topic
