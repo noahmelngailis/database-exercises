7db3969d6ea513adbc945f3bf8d526bb70c429af
Noah Melngailis <noah.melngailis@gmail.com>
2020-08-14 22:19:34 -0500

    working on temporary tables alter and update

---
+++ sql_review/temporary_tables.sql
@@ -0,0 +1,28 @@
+USE curie_941;
+
+-- Using the example from the lesson, re-create the employees_with_departments table.
+CREATE TEMPORARY TABLE emp AS
+SELECT emp_no, first_name, last_name, dept_no, dept_name
+FROM employees.employees
+JOIN employees.dept_emp USING(emp_no)
+JOIN employees.departments USING(dept_no)
+LIMIT 100;
+
+--Add a column named full_name to this table. It should be a VARCHAR whose length is the sum of the lengths of the first name and last name columns
+ALTER TABLE emp ADD full_name VARCHAR(100);
+
+-- Update the table so that full name column contains the correct data
+UPDATE emp
+SET full_name = concat(first_name, " ", last_name);
+
+-- Remove the first_name and last_name columns from the table.
+ALTER TABLE emp DROP COLUMN first_name;
+ALTER TABLE emp DROP COLUMN last_name;
+
+-- Write the SQL necessary to transform the amount column such that it is stored as an integer representing the number of cents of the payment. For example, 1.99 should become 199.
+CREATE TABLE payment AS
+SELECT *
+FROM sakila.payment;
+
+UPDATE payment
+SET amount = cast(round(amount*100, 0) AS BIGINT)
