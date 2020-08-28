c702c068f909fddd5e0dcb10107cb4578fa7937e
Noah Melngailis <noah.melngailis@gmail.com>
2020-08-27 20:48:21 -0500

    world db

--- sql_review/more_exercises.sql
+++ sql_review/more_exercises.sql
@@ -16,3 +16,53 @@ JOIN mean_salary USING (dept_no)
 WHERE employees.salaries.to_date > curdate()
 AND employees.dept_manager.to_date > curdate()
 AND salary < avg_salary;
+
+
+--What languages are spoken in Santa Monica?
+SELECT `Language`, Percentage
+FROM city
+JOIN countrylanguage USING (CountryCode)
+WHERE NAME LIKE "Santa Monica"
+
+--How many different countries are in each region?
+SELECT Region, count(Region) AS num_countries
+FROM country
+GROUP BY Region
+ORDER BY num_countries
+
+-- What is the population for each region?
+SELECT Region, sum(Population) AS population
+FROM country
+GROUP BY Region
+ORDER BY population DESC
+
+-- What is the population for each continent?
+SELECT continent, sum(Population) AS population
+FROM country
+GROUP BY continent
+ORDER BY population DESC
+
+-- What is the average life expectancy globally?
+SELECT AVG(LifeExpectancy)
+FROM country
+
+SELECT sum(LifeExpectancy * Population) / sum(population)
+FROM country
+
+-- What is the average life expectancy for each region, each continent? Sort the results from shortest to longest
+
+SELECT continent, sum(LifeExpectancy * Population) / sum(Population) AS life_expectancy
+FROM country
+GROUP BY continent
+ORDER BY life_expectancy
+
+SELECT Region, sum(LifeExpectancy * Population) / sum(Population) AS life_expectancy
+FROM country
+GROUP BY Region
+ORDER BY life_expectancy
+
+-- Find all the countries whose local name is different from the official name
+SELECT NAME, LocalName,
+	IF (NAME = LocalName, FALSE, TRUE) AS is_named_dif
+FROM country
+WHERE NAME != LocalName
